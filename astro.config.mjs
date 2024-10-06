21c7de7901cdbcfb37e0270a5d6aa682d5d0d09e
Cole Crawford <16374762+ColeDCrawford@users.noreply.github.com>
2024-10-06 18:00:39 -0400

    Add orama

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import pagefind from "astro-pagefind";
+import orama from "@orama/plugin-astro";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://medievalscrolls.com",
     base: "/",
-    integrations: [mdx(), sitemap(), pagefind()],
+    integrations: [mdx(), sitemap(), pagefind(), orama({
+        // We can generate more than one DB, with different configurations
+        scrolls: {
+          pathMatcher: /scrolls\/.+$/,
+          language: "english",
+          contentSelectors: ["main"],
+        },
+      })],
 });
